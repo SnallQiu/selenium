a351a0c6cdeaa19941402a0d593deea540f422ea
SnallQiu <305460936@qq.com>
2018-01-25 22:49:01 +0800

    '卡在了移动滑块太快被检测出来了'

--- taobao/taobao.py
+++ taobao/taobao.py
@@ -18,27 +18,57 @@ class Login:
         driver.get(self.url)
         element_switch = WebDriverWait(driver,60).until(lambda driver:\
                     driver.find_element_by_xpath('//*[@id= "J_Quick2Static"]'))
-        time.sleep(5)
+        time.sleep(2)
         element_switch.click()
         tel_name = driver.find_element_by_name('TPL_username')
         tel_name.send_keys(self.tel)
         time.sleep(5)
         driver.implicitly_wait(10)
+        '''
         password = driver.find_element_by_name('TPL_password')
         password.send_keys(self.passwd)
-        time.sleep(5)
+        time.sleep(2)
+        '''
         '''拖动滑块'''
-        try:
-            first_area = driver.find_element_by_xpath('//span[@id="nc_1_n1z"]')
-            after_area = driver.find_element_by_xpath('//span[@class="nc-lang-cnt"]')
-            ActionChains(driver).drag_and_drop(first_area,after_area).perform()
-        except:
-            pass
+        while True:
+            driver.find_element_by_name('TPL_username').clear()
+            driver.find_element_by_name('TPL_username').send_keys(self.tel)
+            password = driver.find_element_by_name('TPL_password')
+            password.clear()
+            password.send_keys(self.passwd)
+            time.sleep(2)
+
+
+            try:
+                first_area = driver.find_element_by_xpath('//span[@id="nc_1_n1z"]')
+                ActionChains(driver).drag_and_drop_by_offset(first_area,258,0).perform()
+                text = driver.find_element_by_xpath('//*[@id="nc_1__scale_text"]/span')
+                print(text.text)
+                time.sleep(5)
+                if text.text == '验证通过':
+                    break
+                print(driver.find_element_by_xpath('//*[@id="nocaptcha"]/div/span/').text)
+                if driver.find_element_by_xpath('//*[@id="nocaptcha"]/div/span/').text[1] =='哎呀，出错了，点击':
+                    driver.find_element_by_xpath('//*[@id="nocaptcha"]/div/span/a').claik()
+
+            except Exception as e:
+                driver.find_element_by_xpath('//button[@id="J_SubmitStatic"]').click()
+
+
+                #print(str(e))
+
+                pass
+
+
+
         driver.find_element_by_xpath('//button[@id="J_SubmitStatic"]').click()
+        print(driver.get_cookies())
+
+        x
 
 
 
-tel = '17854212463'
+tel = 'qqc琴城'
 password = ''
 mytao = Login(tel,password)
 mytao.login()
